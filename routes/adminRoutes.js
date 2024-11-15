d4791f97b62b3b3c6d7c3f66f57f7608e54da735
oyekunleisaac <oyekunleisaac8@gmail.com>
2024-11-15 06:27:22 +0100

    logout

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerAdmin, loginAdmin } = require('../controllers/adminController');  
+const { registerAdmin, loginAdmin, logoutAdmin } = require('../controllers/adminController');  
+const { authMiddleware } = require('../middleware/authMiddleware'); 
 
 router.post('/signup', registerAdmin);
 router.post('/login', loginAdmin);
+router.post('/logout', authMiddleware, logoutAdmin); 
+// router.post('/logout', authMiddleware, logoutAdmin);
 
 module.exports = router;
