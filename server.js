2a32fb3ac7187d39b7b23d02e4242ae799e4f85f
oyekunleisaac <53318670+oyekunleisaac@users.noreply.github.com>
2024-11-20 20:11:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
+const express = require('express');
+const connectDB = require('./config/db');
 require('dotenv').config();
 const adminRoutes = require('./routes/adminRoutes'); 
 const pmRoutes = require('./routes/pmRoutes'); 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
-console.log(process.env.MONGO_URI);  // This should print the MongoDB URI to the console
-
-// Middleware
-app.use(cors());
-app.use(bodyParser.json()); // For parsing application/json
-
-// Connect to MongoDB Atlas
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, { 
-      useNewUrlParser: true, 
-      useUnifiedTopology: true 
-    });
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1); // Exit the process if MongoDB connection fails
-  }
-};
+app.use(express.json());  
 
 // Connect to MongoDB
 connectDB();
@@ -35,7 +15,6 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/pm', pmRoutes);
 
 
-// Basic Test Route
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
