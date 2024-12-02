cc82bd0afd4bbc2599324bfdaec050713b668c60
oyekunleisaac <53318670+oyekunleisaac@users.noreply.github.com>
2024-12-02 02:53:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const adminRoutes = require('./routes/adminRoutes'); // Import the admin routes
+const adminRoutes = require('./routes/adminRoutes'); 
 const pmRoutes = require('./routes/pmRoutes'); 
+const projectRoutes = require('./routes/projectRoutes'); 
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow any origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Optional, depends on your cookie/session setup
+}));
+
+// const allowedOrigins = ['https://your-production-site.com'];
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,
+// }));
+
+
 app.use(bodyParser.json()); // For parsing application/json
 
 // Connect to MongoDB
@@ -20,8 +39,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.log('MongoDB connection error: ', err));
 
 // Use the admin routes
-app.use('/admin', adminRoutes);
+app.use('/api/admin', adminRoutes);
 app.use('/api/pm', pmRoutes);
+app.use('/api/projects', projectRoutes);
+
 
 // Basic Test Route (You can test this by visiting http://localhost:3000/)
 app.get('/', (req, res) => {
